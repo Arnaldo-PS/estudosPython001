b49a99e7925fbd4c676db2c77e5b7e55942a657e
Arnaldo Pereira Silva <arnaldopsilva6@gmail.com>
2022-06-17 21:21:56 -0300

    add janela GUI

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-import random
+from random import choice
 
 class DecidaPorMim:
     def __init__(self):
@@ -12,9 +12,25 @@ class DecidaPorMim:
         ]
 
     def Iniciar(self):
-        input('Faça sua pergunta: ')
-        print(random.choice(self.respostas))
 
+        layout = [
+            [sg.Text('Faça sua pergunta:')],
+            [sg.Input()],
+            [sg.Output(size=(20,10))],
+            [sg.Button('Decida por mim')]
+        ]
+
+        self.janela = sg.Window('Decida por mim',layout)       
+        
+        while True:
+
+            self.evento, self.valores = self.janela.Read()
+
+            if self.evento == sg.WIN_CLOSED:
+                break
+            elif self.evento == 'Decida por mim':
+                print(choice(self.respostas))
+        
 
 decida = DecidaPorMim()
 decida.Iniciar()
